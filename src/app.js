675340cd0e65593ff65eec5c272736785fa62a0e
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2017-01-02 17:20:40 +0100

    Fixed indentation

--- src/app.js
+++ src/app.js
@@ -5,34 +5,35 @@ const SENSORS_URL = 'http://powietrze.gios.gov.pl/pjp/rest/station/sensors/';
 const READINGS_URL = 'http://powietrze.gios.gov.pl/pjp/rest/data/getData/';
 
 function print(object) {
-    console.info(JSON.stringify(object, null, 2));
+  console.info(JSON.stringify(object, null, 2));
 }
 
 async function fetchData() {
-    const stations = await fetchStations();
-    for (let station of stations) {
-        print(station);
-        const sensors = await fetchSensors(station.id);
-        for (let sensor of sensors) {
-            print(sensor);
-            const readings = await fetchReadings(sensor.id);
-            print(readings);
-        }
+  const stations = await fetchStations();
+  for (let station of stations) {
+    print(station);
+    const sensors = await fetchSensors(station.id);
+    for (let sensor of sensors) {
+      print(sensor);
+      const readings = await fetchReadings(sensor.id);
+      print(readings);
     }
+  }
 }
 
 function fetchStations() {
-    return fetch(STATIONS_URL).then((response) => response.json());
+  return fetch(STATIONS_URL).then((response) => response.json());
 }
 
 function fetchSensors(stationId) {
-    return fetch(SENSORS_URL + stationId).then((response) => response.json());
+  return fetch(SENSORS_URL + stationId).then((response) => response.json());
 }
 
 function fetchReadings(sensorId) {
-    return fetch(READINGS_URL + sensorId).then((response) => response.json());
+  return fetch(READINGS_URL + sensorId).then((response) => response.json());
 }
 
 fetchData()
-    .then(() => console.info('Done.'))
-    .catch((error) => console.error(error));
+  .then(() => console.info('Done.'))
+  .then(() => console.log('process.env', process.env))
+  .catch((error) => console.error(error));
